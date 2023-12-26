37ec0fe9d309530a4ad112b53f67e259469f2a9e
Alpay Özer <alpayozer7@gmail.com>
2023-12-26 22:08:55 +0300

    temel yapı kuruldu

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SafeAreaView, TouchableOpacity, View, Text} from 'react-native';
+
+const Button = () => {
+  return (
+    <SafeAreaView>
+      <Text>Button</Text>
+    </SafeAreaView>
+  );
+};
+
+export default Button;
