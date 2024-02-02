efdf80104c7eb7767ec49ed5aeef8f1e41ec47de
Alpay Özer <alpayozer7@gmail.com>
2024-02-03 00:19:54 +0300

    homeScreen dataları ayarlandı

---
+++ src/css/Home.style.ts
@@ -0,0 +1,44 @@
+import { StyleSheet } from 'react-native';
+
+
+export default StyleSheet.create({
+    container: {
+        backgroundColor: "white",
+        flex: 1
+
+    },
+    welcome: {
+        fontSize: 15,
+        color: "#0F6BE9",
+        paddingTop: 30,
+
+    },
+    prescription: {
+        fontSize: 14,
+        color: "#0F6BE9",
+    },
+    image: {
+        height: 80,
+        width: 80,
+        borderRadius: 50,
+    },
+    top_container: {
+        borderWidth: 1,
+        borderColor: "#0F6BE9",
+        borderBottomLeftRadius: 50,
+        borderBottomRightRadius: 50,
+        alignItems: "center",
+        backgroundColor: 'rgba(15, 107, 233, 0.15)'
+    },
+    image_container: {
+        backgroundColor: "white",
+        width: 90,
+        height: 90,
+        borderRadius: 50,
+        alignItems: "center",
+        justifyContent: "center",
+        top: 40,
+    }
+
+
+})
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,10 +1,39 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Image, Text, View} from 'react-native';
+import userData from '../assets/data/user.json';
+import styles from '../css/Home.style';
+import HomePrescriptionBox from '../components/HomePrescriptionBox/HomePrescriptionBox';
+import prescriptionData from '../assets/data/prescription.json';
 
 const HomeScreen = () => {
+  const user = userData[0];
+
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+      <View style={styles.top_container}>
+        <Text style={styles.welcome}>Welcome {user.name}</Text>
+        <Text style={styles.prescription}>Bugün randevun yok</Text>
+        <View style={styles.image_container}>
+          <Image style={styles.image} source={{uri: user.image}} />
+        </View>
+      </View>
+      <View>
+        <Text>Takvim</Text>
+      </View>
+      <View>
+        <Text>Randevularım</Text>
+        <View>
+          {prescriptionData.map(item => (
+            <HomePrescriptionBox
+              key={item.id}
+              name={item.doctor_name}
+              hospital={item.doctor_hospital}
+              date={item.prescription_date}
+              time={item.prescription_hour}
+            />
+          ))}
+        </View>
+      </View>
     </View>
   );
 };
